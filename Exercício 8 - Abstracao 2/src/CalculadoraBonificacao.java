48735142e85565da2f00aefb88fc5a12d3dee319
Ferreira <98367479+alexfsxy@users.noreply.github.com>
2024-10-06 03:26:42 -0300

    Create CalculadoraBonificacao.java

---
+++ Exercício 8 - Abstracao 2/src/CalculadoraBonificacao.java
@@ -0,0 +1,9 @@
+public class CalculadoraBonificacao {
+
+    public double calcularBonificacao(Colaborador colaborador){
+
+       double salarioBase = colaborador.salario();
+
+        return colaborador.cargo().calcularBonificacao(salarioBase);
+    }
+}
